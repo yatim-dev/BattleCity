db3973da9317477a7e0cb51863cc92c7ff8b8fc7
Yarkov Timothy <yatim2002@gmail.com>
2022-11-29 15:47:34 +0500

    Initial commit

---
+++ core/src/com/mygdx/game/screens/Play.java
@@ -0,0 +1,62 @@
+package com.mygdx.game.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+public class Play implements Screen {
+
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+    private OrthographicCamera camera;
+
+    @Override
+    public void show() {
+        map = new TmxMapLoader().load("level1/level1.tmx");
+
+        renderer = new OrthogonalTiledMapRenderer(map);
+        camera = new OrthographicCamera();
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        renderer.setView(camera);
+        renderer.render();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        camera.viewportWidth = width;
+        camera.viewportHeight = height;
+        camera.setToOrtho(false, 832, 864);
+        camera.update();
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+        dispose();
+    }
+
+    @Override
+    public void dispose() {
+        map.dispose();
+        renderer.dispose();
+    }
+}
